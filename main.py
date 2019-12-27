0bb24f5e8c5629ca537314cd2ff714cfbf9dcb61
RetroRaisins <retroraisins@gmail.com>
2019-12-27 15:45:49 +0300

    [upd] get user videos list

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
-from threading import Thread
-from conf import VIDEOS_COUNT
+# from threading import Thread
+from conf import HEADERS, PAYLOAD
+import requests
+import json
+import re
+import logging
 
+
+logger = logging.getLogger()
 ERROR = 'Count can''t be negative'
+tiktok_url = 'https://tikitoks.com/'
 
 
-def download_user_videos(username=None, video_cn=VIDEOS_COUNT):
-    if 1 < video_cn <= VIDEOS_COUNT:
-        for _ in range(video_cn):
-            create_downloading_thread()
+def download_user_videos(username=None):
+    if not username:
+        print('Username not found')
     else:
-        print(ERROR)
+        user_homepage_url = ''.join([tiktok_url, '@', username])
+        try:
+            content = requests.post("https://tikitoks.com/@" + username,
+                                    data=json.dumps(PAYLOAD),
+                                    headers=HEADERS).content
+        except Exception as e:
+            logger.error(e)
+        else:
+
+            video_pattern = re.compile(b'/video/[0-9]+')
+            user_videos = re.findall(video_pattern, content)
+            print('Videos count: {}'.format(len(user_videos)))
+
+    # if 1 < video_cn <= VIDEOS_COUNT:
+    #     for _ in range(video_cn):
+    #         create_downloading_thread()
+    # else:
+    #     print(ERROR)
 
 
 def download_video():
@@ -17,4 +40,9 @@ def download_video():
 
 
 def create_downloading_thread():
-    thread = Thread(download_video, args=())
+    pass
+    # thread = Thread(download_video, args=())
+
+
+tiktok_user = 'egorkreed'
+download_user_videos(tiktok_user)
