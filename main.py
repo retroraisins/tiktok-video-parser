99f05c41db824285676ade16836711c15a2b994d
RetroRaisins <retroraisins@gmail.com>
2019-12-30 17:33:16 +0300

    [del] ms edge webdriver testing

--- main.py
+++ main.py
@@ -19,19 +19,6 @@ HEADERS = {
     }
 
 
-def test_msedge_driver():
-    import time
-
-    driver = webdriver.Edge()  # Optional argument, if not specified will search path.
-    driver.get('http://www.google.com/')
-    time.sleep(5)  # Let the user actually see something!
-    search_box = driver.find_element_by_name('q')
-    search_box.send_keys('EdgeDriver')
-    search_box.submit()
-    time.sleep(5)  # Let the user actually see something!
-    driver.quit()
-
-
 def test_chrome_driver():
     import time
 
