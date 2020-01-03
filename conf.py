2c84b0667e5a29e759b5b788727dcfdb19ca2acf
RetroRaisins <retroraisins@gmail.com>
2020-01-03 14:59:12 +0300

    [upd] replace configs to file

---
+++ conf.py
@@ -0,0 +1,13 @@
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+    }
+
+PROXIES = {
+    'http': 'http://79.190.145.141:3128',
+    'https': 'https://95.79.57.206:53281'
+}
+
+SRC_FILE_PATH = './sources/video_src.txt'
+URLS_FILE_PATH = './urls/video_urls.txt'
+VIDEOS_FILES_PATH = './videos/'
+FRAMES_FILES_PATH = 'frames/'
