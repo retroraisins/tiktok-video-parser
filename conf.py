c7b3ca95bba325eb073587fecb4efe3f9c381ae8
RetroRaisins <retroraisins@gmail.com>
2020-01-03 15:06:49 +0300

    [upd] add const with chrome profile location

--- conf.py
+++ conf.py
@@ -2,6 +2,8 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
     }
 
+CHROME_PROFILE_LOCATION = '/Users/RRustam/Library/Application Support/Google/Chrome/'
+
 PROXIES = {
     'http': 'http://79.190.145.141:3128',
     'https': 'https://95.79.57.206:53281'
