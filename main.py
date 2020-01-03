895e6354aff9d25a08286c1378a27ea0628ca5a8
RetroRaisins <retroraisins@gmail.com>
2020-01-03 22:23:59 +0300

    [upd] change to return array of dicts

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from lxml import html
 import logging
 import os
 import sys
-from conf import HEADERS, PROXIES, SRC_FILE_PATH,   \
+import json
+from conf import HEADERS, PROXIES,  \
     URLS_FILE_PATH, FRAMES_FILES_PATH, \
     VIDEOS_FILES_PATH
 
@@ -19,6 +20,7 @@ LINKS_NOT_FOUND_OR_SERVER_ERR = 'Links videos not found. Or it is possible ' \
 
 
 def download_user_videos(username=None):
+    result = []
 
     def get_user_id_from_tree(tree):
         hrefs = tree.xpath('//a/@href')
@@ -60,6 +62,56 @@ def download_user_videos(username=None):
                 sys.exit(LINKS_NOT_FOUND_OR_SERVER_ERR)
             return user_video_urls
 
+    def get_first_frame(in_file_path):
+        out_filename = ''.join([os.path.basename(in_file_path).split('.')[0], '.jpg'])
+        os.makedirs(os.path.dirname(FRAMES_FILES_PATH), exist_ok=True)
+        out = ''.join([os.path.abspath(FRAMES_FILES_PATH), '/', out_filename])
+        print('frames to:', out)
+        ffmpg_cmd = 'ffmpeg -i {} -ss 00:00:00.000 -vframes 1 {}'.format(
+            in_file_path, out)
+        os.system(ffmpg_cmd)
+
+    def get_video_src(url):
+        page = requests.get(url, headers=HEADERS)
+        tree = html.fromstring(page.content)
+        source = tree.xpath('//video/@src')
+        download_video(source[0], url)
+
+    def put_urls_in_file(urls):
+        os.makedirs(os.path.dirname(URLS_FILE_PATH), exist_ok=True)
+        with open(URLS_FILE_PATH, 'w') as f:
+            for url in urls:
+                print(url, file=f)
+
+    def threading_requests(items_to_request, func):
+        threads = []
+        for item in items_to_request:
+            process = Thread(target=func, args=[item])
+            process.start()
+            threads.append(process)
+        for process in threads:
+            process.join()
+
+        return result
+
+    def download_video(src, url):
+        BEGIN = 24
+        END = 44
+        r = requests.get(src)
+        logging.info('start downloading from {}'.format(src[0:32]))
+        filename = src[BEGIN:END] + '.mp4'
+        save_to = VIDEOS_FILES_PATH + filename
+        os.makedirs(os.path.dirname(save_to), exist_ok=True)
+        with open(save_to, 'wb') as f:
+            f.write(r.content)
+        result.append({
+            'src': src,
+            'video_url': url,
+            'first_frame_path': save_to
+        })
+        get_first_frame(save_to)
+        logging.info('finished')
+
     if not username:
         logger.warning('Username not found')
     else:
@@ -69,55 +121,9 @@ def download_user_videos(username=None):
         tiktok_video_urls = list(map(
             lambda _id: ''.join([TIKTOK_URL, DOG, username,  '/video/', _id]),
             tikitoks_video_ids))
-        put_urls_in_file(tiktok_video_urls)
-        threading_requests(tiktok_video_urls, get_video_src)
+        # put_urls_in_file(tiktok_video_urls)
+        return threading_requests(tiktok_video_urls, get_video_src)
 
 
-def get_first_frame(in_file_path):
-    out_filename = ''.join([os.path.basename(in_file_path).split('.')[0], '.jpg'])
-    os.makedirs(os.path.dirname(FRAMES_FILES_PATH), exist_ok=True)
-    out = ''.join([os.path.abspath(FRAMES_FILES_PATH), '/', out_filename])
-    print('frames to:', out)
-    ffmpg_cmd = 'ffmpeg -i {} -ss 00:00:00.000 -vframes 1 {}'.format(
-        in_file_path, out)
-    os.system(ffmpg_cmd)
-
-
-def get_video_src(url):
-    page = requests.get(url, headers=HEADERS)
-    tree = html.fromstring(page.content)
-    source = tree.xpath('//video/@src')
-    os.makedirs(os.path.dirname(SRC_FILE_PATH), exist_ok=True)
-    with open(SRC_FILE_PATH, 'a') as f:
-        f.write(source[0])
-    download_video(source[0])
-
-
-def put_urls_in_file(urls):
-    os.makedirs(os.path.dirname(URLS_FILE_PATH), exist_ok=True)
-    with open(URLS_FILE_PATH, 'w') as f:
-        for url in urls:
-            print(url, file=f)
-
-
-def threading_requests(items_to_request, func):
-    threads = []
-    for item in items_to_request:
-        process = Thread(target=func, args=[item])
-        process.start()
-        threads.append(process)
-    for process in threads:
-        process.join()
-
-
-def download_video(url):
-    r = requests.get(url)
-    logging.info('start downloading from {}'.format(url[0:32]))
-    filename = url[24:44] + '.mp4'
-    save_to = VIDEOS_FILES_PATH + filename
-    os.makedirs(os.path.dirname(save_to), exist_ok=True)
-    with open(save_to, 'wb') as f:
-        f.write(r.content)
-    get_first_frame(save_to)
-    logging.info('finished')
-
+res = download_user_videos('egorkreed')
+print(json.dumps(res, indent=2))
